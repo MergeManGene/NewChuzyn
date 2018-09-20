5ba9e4032c2576b2b11bcdafb3200ff50768ca2b
DA\nwuser <noppo.kappa@gmail.com>
2018-09-21 02:25:47 +0900

    [Input]タッチスクリプト導入

---
+++ NewChuzyn/Assets/Scripts/Framework/InputManager.cs
@@ -0,0 +1,16 @@
+﻿//概要：TouchZoneから各インプット動作を受け取り格納するクラス
+//参考：なし
+//担当：海發
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InputManager : MonoBehaviour {
+
+   [SerializeField]
+   private TouchZone m_touchZone;
+
+
+}
---
+++ NewChuzyn/Assets/Scripts/Framework/TouchZone.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TouchScript.Gestures;
+
+public class TouchZone : MonoBehaviour {
+
+    /// <summary>タッチ開始座標 </summary>
+    [HideInInspector]
+    public Vector3 pressStartPosition;
+
+    /// <summary>Press用フラグ </summary>
+    [HideInInspector]
+    public bool pressing, releasing,tapping;
+
+    /// <summary>
+    /// 開始処理
+    /// delegateに登録
+    /// </summary>
+    private void OnEnable(){
+        GetComponent<PressGesture>().Pressed += PressedHandle;
+        GetComponent<TapGesture>().Tapped += TapeedHandle;
+        GetComponent<ReleaseGesture>().Released += ReleasedHandle;
+    }
+
+    private void OnDisable(){
+        UnsubscribeEvent();
+    }
+
+    void OnDestroy(){
+        UnsubscribeEvent();
+    }
+
+    void UnsubscribeEvent(){
+        // 登録を解除
+        GetComponent<PressGesture>().Pressed += PressedHandle;
+    }
+
+    //タップ処理
+    void PressedHandle(object arg_sender, System.EventArgs arg_e){
+        var send = arg_sender as PressGesture;
+        pressStartPosition = send.ScreenPosition;
+        Debug.Log("プレス");
+
+    }
+
+    //リリース処理
+    void ReleasedHandle(object arg_sender, System.EventArgs arg_e){
+        Debug.Log("リリース");
+    }
+
+    void TapeedHandle(object arg_sender, System.EventArgs arg_e){
+        var send = arg_sender as TapGesture;
+        Debug.Log("タップ");
+    }
+}
