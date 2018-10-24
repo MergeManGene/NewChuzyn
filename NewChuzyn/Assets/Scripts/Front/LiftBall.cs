ad122d7ed953e32725ca1adfcbea4e18e84cf6b5
DA\nwuser <noppo.kappa@gmail.com>
2018-10-24 22:31:24 +0900

    【ギミック】リフトギミック実装

---
+++ NewChuzyn/Assets/Scripts/Front/LiftBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftBall : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_endMarker;
+
+    private float m_liftSpeed = 0.05f;
+
+    [SerializeField]
+    private Switch m_switch;
+
+    //リフトのボールを動かすだけ
+    private void LiftMove(){
+        if(m_switch.pushing)
+        transform.position = Vector3.Lerp(transform.position, m_endMarker.transform.position, 0.05f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        LiftMove();
+	}
+}
---
+++ NewChuzyn/Assets/Scripts/Front/Switch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    /// <summary>スイッチフラグ</summary>
+    public bool pushing;
+
+    private SpriteRenderer m_spriteRenderer;
+
+    private void Start(){
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    //スイッチ押されたら赤くなる
+    private　void PushSwich(){
+        pushing = true;
+        m_spriteRenderer.color = Color.red;
+    }
+    private void OnTriggerEnter2D(Collider2D arg_col){
+        PushSwich();
+    }
+}
