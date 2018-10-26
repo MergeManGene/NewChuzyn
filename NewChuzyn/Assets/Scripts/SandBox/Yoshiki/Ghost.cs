7370268bd75592352c2e67cfc4b6e2c0f5c2f053
DA\nwuser <ysk1033738432@gmail.com>
2018-10-26 10:23:37 +0900

    【stage2】ほぼ完成

--- NewChuzyn/Assets/Scripts/SandBox/Yoshiki/Ghost.cs
+++ NewChuzyn/Assets/Scripts/SandBox/Yoshiki/Ghost.cs
@@ -16,38 +16,70 @@ public class Ghost : MonoBehaviour {
     [SerializeField]
     private float m_speed;
 
+    /// <summary>
+    /// 縦移動もしくは横移動
+    /// デフォは横
+    /// </summary>
+    [SerializeField]
+    private bool m_sideMove = true;
+
+    private Vector2 m_position;
+
     private Rigidbody2D m_rigidbody2d;
 
 	private void Start () {
         m_rigidbody2d = GetComponent<Rigidbody2D>();
-        StartCoroutine("Move");
+
+        if (m_sideMove)
+        {
+            StartCoroutine("MoveSide");
+        }
+        else
+        {
+            StartCoroutine("MoveVertical");
+        }
+        
+        m_position = transform.position;
 	}
 	
 	private void Update () {
         
 	}
 
-    private IEnumerator Move()
+    private IEnumerator MoveSide()
     {
-        int count = 0;
         
         while (true)
         {
-            if (count > 5)
+
+            if (m_position.x + m_distance < transform.position.x)
             {
-                m_rigidbody2d.velocity += (Vector2.left * m_speed) * Time.fixedDeltaTime;
-                
+                m_rigidbody2d.velocity += (Vector2.left * m_speed);
+
             }
-            else
+            else if (m_position.x + m_distance > transform.position.x) 
             {
-                m_rigidbody2d.velocity -= (Vector2.left * m_speed) * Time.fixedDeltaTime;
+                m_rigidbody2d.velocity -= (Vector2.left * m_speed);
             }
 
-            count++;
+            yield return new WaitForSeconds(1f);
+        }
+    }
 
-            if (count > 10) 
+    private IEnumerator MoveVertical()
+    {
+
+        while (true)
+        {
+
+            if (m_position.y + m_distance < transform.position.y)
             {
-                count = 1;
+                m_rigidbody2d.velocity += (Vector2.down * m_speed);
+
+            }
+            else if (m_position.y + m_distance > transform.position.y)
+            {
+                m_rigidbody2d.velocity -= (Vector2.down * m_speed);
             }
 
             yield return new WaitForSeconds(1f);
