c33e7c1b57f92655c2150b3423021be997ae83d0
DA\nwuser <noppo.kappa@gmail.com>
2018-10-26 23:49:59 +0900

    【Player】幽霊の音邪

--- NewChuzyn/Assets/Scripts/Front/UI/PassWall.cs
+++ NewChuzyn/Assets/Scripts/Front/UI/PassWall.cs
@@ -7,11 +7,13 @@ public class PassWall : MonoBehaviour {
     /// <summary>タイルマップコライダー</summary>
     private Collider2D m_collider2D;
 
+    private AudioSource m_audioSource;
     /// <summary>
     /// タイルマップコライダーのコンポーネント取得
     /// </summary>
     private void Start(){
         m_collider2D = GetComponent<Collider2D>();
+        m_audioSource = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter2D(Collision2D arg_col){
@@ -31,6 +33,11 @@ public class PassWall : MonoBehaviour {
         if (m_collider2D.isTrigger){
             m_collider2D.isTrigger = false;
             GameManager.PlayerFormInstanse = GameManager.PlayerFormState.Normal;
+
+            //音邪
+            if (!m_audioSource.isPlaying){
+                m_audioSource.Play();
+            }
         }
     }
 }
