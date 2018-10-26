e7a11cec881eacee1102ecba0324bf7214912d9c
DA\nwuser <noppo.kappa@gmail.com>
2018-10-27 07:22:34 +0900

    【Input】実機インプット対応

--- NewChuzyn/Assets/Scripts/Front/StorySkip.cs
+++ NewChuzyn/Assets/Scripts/Front/StorySkip.cs
@@ -8,31 +8,55 @@ public class StorySkip : MonoBehaviour
 
     public enum SceneType { Title, Prologe,Story1,Ending }
     public SceneType m_sceneType;
+
     // Update is called once per frame
     void Update()
     {
+
+
+        // エディタの場合の処理     
         //ボタン押したときシーンに合わせて次のシーンへ移動
-        if (Input.GetMouseButtonDown(0))
+        if (Application.isEditor)
         {
-            if (m_sceneType == SceneType.Prologe)
+            if (Input.GetMouseButtonDown(0))
             {
-                FadeManager.Instance.LoadScene("Stage1", 1f);
+                InputSceneAction();
             }
-
-            if (m_sceneType == SceneType.Title)
+        }
+        else
+        {
+            // エディタ以外の場合
+            if (Input.touchCount > 0)
             {
-                FadeManager.Instance.LoadScene("Prologue", 1f);
-            }
-
-            if (m_sceneType == SceneType.Story1)
-            {
-                FadeManager.Instance.LoadScene("Stage2", 1f);
-            }
-
-            if (m_sceneType == SceneType.Ending)
-            {
-                FadeManager.Instance.LoadScene("Title", 1f);
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    InputSceneAction();
+                }
             }
         }
     }
+
+    private void InputSceneAction(){
+        if (m_sceneType == SceneType.Prologe)
+        {
+            FadeManager.Instance.LoadScene("Stage1", 1f);
+        }
+
+        if (m_sceneType == SceneType.Title)
+        {
+            FadeManager.Instance.LoadScene("Prologue", 1f);
+        }
+
+        if (m_sceneType == SceneType.Story1)
+        {
+            FadeManager.Instance.LoadScene("Stage2", 1f);
+        }
+
+        if (m_sceneType == SceneType.Ending)
+        {
+            FadeManager.Instance.LoadScene("Title", 1f);
+        }
+    }
 }
+
+
