a979277ae990e83f0551f1feadda615df060b4a0
DA\nwuser <puzz_ryu_777@ezweb.ne.jp>
2018-11-30 15:36:55 +0900

    【ちんぱんぷっしゅ】

---
+++ NewChuzyn/Assets/Scenes/SandBox/Inagaki/Script/Stage_Select.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage_Select : MonoBehaviour {
+    public GameObject selectObject;
+
+
+    
+    /// <summary>
+    /// TitleBackボタン呼び出し用
+    /// </summary>
+    public void BackTitle(){
+        FadeManager.Instance.LoadScene("Title", 0.5f);
+    }
+
+    public void stg_1(){
+        FadeManager.Instance.LoadScene("Stage1", 1f);
+    }
+    public void stg_2()
+    {
+        FadeManager.Instance.LoadScene("Stage2", 1f);
+    }
+
+}
