9b8caed1f24c87ef2c8cdbf61fd53c56408cf4e3
DA\nwuser <noppo.kappa@gmail.com>
2019-01-25 09:55:07 +0900

    【サーバー】くっそおもいモロモロ

---
+++ NewChuzyn/Assets/Scripts/Front/UserData/UDhoge.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class UDhoge : MonoBehaviour {
+
+    private DatabaseReference userDB;
+
+    // Use this for initialization
+    void Start()
+    {
+        //データベースURLを設定
+        FirebaseApp.DefaultInstance.SetEditorServiceAccountEmail("https://new-chuzyn.firebaseio.com/");
+        userDB = FirebaseDatabase.DefaultInstance.GetReference("userdata");
+        Debug.Log("へい" + userDB);
+
+        //userDB.Child("age").GetValueAsync().ContinueWith(task=>)
+    }
+}
