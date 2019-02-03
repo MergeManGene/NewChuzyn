f320735a51b21c059fbbfa12f08406b80c46b0b7
DA\nwuser <noppo.kappa@gmail.com>
2019-02-04 02:27:20 +0900

    【Firebase】とりあえずデータ取得と書き込みできた

--- NewChuzyn/Assets/Scripts/Front/UserData/UDhoge.cs
+++ NewChuzyn/Assets/Scripts/Front/UserData/UDhoge.cs
@@ -17,6 +17,47 @@ public class UDhoge : MonoBehaviour {
         userDB = FirebaseDatabase.DefaultInstance.GetReference("userdata");
         Debug.Log("へい" + userDB);
 
-        //userDB.Child("age").GetValueAsync().ContinueWith(task=>)
+        //ageのノードからtimeで昇順ソートして最大10件を取る（非同期)
+        userDB.Child("userid").GetValueAsync().ContinueWith(task =>
+        {
+            //取得失敗時
+            if (task.IsFaulted){
+                Debug.Log("取得失敗");
+            }//取得成功
+            else if (task.IsCompleted){
+                Debug.Log("取得成功");
+                
+                //取得結果
+                DataSnapshot snapshot = task.Result;
+
+                //取得の仕方例：
+                //snapshot.Child("何番か数値").Child("").GetValue(true)));
+                Debug.Log(snapshot.Child("1").Child("age").GetValue(true));
+
+
+                //書き込み
+                //ノードにレコードを登録(Push)して生成されたキーを取得
+                string key = userDB.Child("userid").Push().Key;
+
+                //登録する一件データをDictionaryで定義(nameとtime)
+                Dictionary<string, object> itemMap = new Dictionary<string, object>();
+                itemMap.Add("age", "22");
+                itemMap.Add("job", "学生");
+                itemMap.Add("sex", "女性");
+
+                //itemMap.Add(snapshot.Child("1").GetValue(true).ToString(), name);
+
+                Dictionary<string, object> map = new Dictionary<string, object>();
+
+                // "/"で一段落とせる
+                map.Add(1 + "/" + key, itemMap);
+
+                //データ更新
+                userDB.UpdateChildrenAsync(map);
+
+                //結果リストemulatorで処理
+                IEnumerator<DataSnapshot> en = snapshot.Children.GetEnumerator();
+            }
+        });
     }
 }
