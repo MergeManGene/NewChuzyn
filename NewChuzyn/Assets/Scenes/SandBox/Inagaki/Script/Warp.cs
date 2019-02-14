eb2c8d6a4d324e8a965ed4fdef4500ce74359d55
DA\nwuser <puzz_ryu_777@ezweb.ne.jp>
2019-02-14 16:06:47 +0900

    【ファイナルプッシュ】

---
+++ NewChuzyn/Assets/Scenes/SandBox/Inagaki/Script/Warp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+    public GameObject object1;
+    bool flg;
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            transform.position = object1.transform.position;
+        }
+    }
+   
+   
+}
